65c9f2484fc3f5cfd194ecaf8e3748f94e7b295c
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-08-16 00:25:18 -0400

    added duplicateArrays questions

---
+++ 7.duplicateArrays.js
@@ -0,0 +1,16 @@
+Array.prototype.duplicate = function() {
+  
+  var len = this.length;
+  
+  for (var i = 0; i < len; i++) {
+    this[len + i] = this[i];
+  }
+  
+  return this;
+}
+  
+Array.prototype.duplicate = function () {
+    var array = this;
+    return array.concat(array);
+};
+ console.log([1,2,3,4,5].duplicate()); //[1,2,3,4,5,1,2,3,4,5]
