7c9675619312b88def1aedfa612e7fed30a7702f
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-08-18 17:14:32 -0400

    added sortArray

--- 8.sortArray.js
+++ 8.sortArray.js
@@ -1,12 +1,24 @@
-//sorting an array with ascending orer
+//sorting an array with descending orer
 
 let Arr = [1, 7, 2, 8, 3, 4, 5, 0, 9];
 
-for (var i = 1; i < Arr.length; i++)
-  for (var j = 0; j < i; j++)
+for (let i = 0; i < Arr.length; i++)
+  for (let j = i + 1; j < Arr.length; j++)
     if (Arr[i] < Arr[j]) {
-      var x = Arr[i];
+      let x = Arr[i];
       Arr[i] = Arr[j];
       Arr[j] = x;
     }
-    console.log(Arr);
+console.log(Arr);
+
+//sorting an array with descending orer
+let Arr2 = [1, 7, 2, 8, 3, 4, 5, 0, 9];
+
+for (let i = 0; i < Arr2.length; i++)
+  for (let j = i + 1; j < Arr2.length; j++)
+    if (Arr[i] > Arr[j]) {
+      let x = Arr[i];
+      Arr[i] = Arr[j];
+      Arr[j] = x;
+    }
+console.log(Arr2);
