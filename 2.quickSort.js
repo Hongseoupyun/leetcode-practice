c91a979c248bac1d97f636892a52a12192f0bf80
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-08-18 13:17:41 -0400

    added quicksort algorighms

--- 2.quickSort.js
+++ 2.quickSort.js
@@ -8,23 +8,23 @@ function quicksort(array) {
   }
   const pivot = array[array.length - 1];
   const leftArr = [];
-  const rightArr= [];
-  for (let i = 0; i < array.length - 1; i++){
-    if(array[i]<pivot){
-      leftArr.push(array[i])
+  const rightArr = [];
+  for (let i = 0; i < array.length - 1; i++) {
+    if (array[i] < pivot) {
+      leftArr.push(array[i]);
     } else {
-      rightArr.push(array[i])
+      rightArr.push(array[i]);
     }
   }
-  if (leftArr.length > 0 && rightArr.length>0){
-    return [...quicksort(leftArr), pivot, ...quicksort(rightArr)]
-  } else if (leftArr.length>0) {
-    return [...quicksort(leftArr), pivot]
+  if (leftArr.length > 0 && rightArr.length > 0) {
+    return [...quicksort(leftArr), pivot, ...quicksort(rightArr)];
+  } else if (leftArr.length > 0) {
+    return [...quicksort(leftArr), pivot];
   } else {
-    return [pivot, ...quicksort(rightArr)]
+    return [pivot, ...quicksort(rightArr)];
   }
 }
 
-const arr = [1,4,2,8,345,13,43,34,5643,64,123,43,2,55,1234,92] 
+const arr = [1, 4, 2, 8, 345, 13, 43, 34, 5643, 64, 123, 43, 2, 55, 1234, 92];
 
-console.log(quicksort(arr))
+console.log(quicksort(arr));
