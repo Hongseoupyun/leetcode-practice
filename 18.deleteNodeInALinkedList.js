ae808cac56b4925659fc5d5e71fbc586fd2fcb42
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-08-27 23:44:10 -0400

    added comments on deletenodeinlinkedlist

--- 18.deleteNodeInALinkedList.js
+++ 18.deleteNodeInALinkedList.js
@@ -9,8 +9,19 @@
  * @param {ListNode} node
  * @return {void} Do not return anything, modify node in-place instead.
  */
- var deleteNode = function(node) {
-    let next = node.next.next;
-    node.val = node.next.val;
-    node.next = next;
+
+
+
+  //change our current node to next node and set next node.value = node.next.val
+  
+ const deleteNode = function(node) {
+
+  //4-5-1-9 given node = 5
+    //have a reference to next node
+    let next = node.next.next;//9
+    //set current node.val as next node.val
+    node.val = node.next.val;//1
+    //set node.next and node.next.next
+    node.next = next;//9
+    //4-1-9
 };
