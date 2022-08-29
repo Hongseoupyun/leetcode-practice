cbb7d383e292120f1ea00cc406e3092beb903f32
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-08-29 00:12:15 -0400

    commit

--- 10.anagram.js
+++ 10.anagram.js
@@ -2,17 +2,14 @@
 
 //2.If you order two anaram words in alphabetical order it will be uniform
 
-
 const isAnagram1 = function (string1, string2) {
-  let string1Sorted = string1.toLowerCase().split("").sort().join()
-  let string2Sorted = string2.toLowerCase().split("").sort().join()
+  let string1Sorted = string1.toLowerCase().split("").sort().join();
+  let string2Sorted = string2.toLowerCase().split("").sort().join();
 
-  console.log(string1Sorted)
-  console.log(string2Sorted)
- return string1Sorted === string2Sorted
-
-}
-
+  console.log(string1Sorted);
+  console.log(string2Sorted);
+  return string1Sorted === string2Sorted;
+};
 
 //using hashmap
 
@@ -22,26 +19,25 @@ const isAnagram1 = function (string1, string2) {
 //4.if the letter and count is not same , it is not anagram
 //5.otherwise, reture true
 const isAnagram2 = function (string1, string2) {
-  if (string1.length !== string2.length) return false
+  if (string1.length !== string2.length) return false;
 
-  let string1Count = {}
-  let string2Count = {}
+  let string1Count = {};
+  let string2Count = {};
 
   for (let i = 0; i < string1.length; i++) {
     if (!string1Count[string1.toLowerCase()[i]]) {
-      string1Count[string1.toLowerCase()[i]] = 1
+      string1Count[string1.toLowerCase()[i]] = 1;
     }
     if (!string2Count[string2.toLowerCase()[i]]) {
-      string2Count[string2.toLowerCase()[i]] = 1
+      string2Count[string2.toLowerCase()[i]] = 1;
     }
   }
 
   for (let key in string1Count) {
-    if (string1Count[key] !== string2Count[key]) return false
+    if (string1Count[key] !== string2Count[key]) return false;
   }
-  console.log(string1Count)
-  return true
-}
+  return true;
+};
 
-console.log(isAnagram1("Cineman", "icemann"))
-console.log(isAnagram2("Cineman", "icemann"))
+console.log(isAnagram1("Cineman", "icemann"));
+console.log(isAnagram2("Cinema", "icemann"));
