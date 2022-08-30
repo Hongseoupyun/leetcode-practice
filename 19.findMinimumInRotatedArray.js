6f45d733fd5d48279892dbc2dc9198b029c98754
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-08-30 01:39:47 -0400

    added findMinimumInRotatedArray.js

---
+++ 19.findMinimumInRotatedArray.js
@@ -0,0 +1,29 @@
+//You must write an algorithm that runs in O(log n) time.
+//binary search
+// //Input: nums = [3,4,5,1,2]
+// Output: 1
+// Explanation: The original array was [1,2,3,4,5] rotated 3 times.
+
+function findMin(nums) {
+  let left = 0
+  let right = nums.length - 1
+  
+  while (left < right) {   
+    let midPoint = Math.floor((left + right) / 2)
+    if (nums[midPoint] > nums[right]) {
+      left = midPoint + 1
+    } else {
+      right = midPoint
+    }
+  }
+  return nums[left]
+}
+
+
+//O(n)example
+
+function findMin3(nums) {
+  return Math.min(...nums)
+}
+
+console.log(findMin([3,4,5,1,2]))
