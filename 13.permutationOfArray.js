a54cf018c4e26ef722b6ca5109cd5e66350d26d6
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-09-01 01:08:35 -0400

    added algorithms in permutationOfArrays.js

---
+++ 13.permutationOfArray.js
@@ -0,0 +1,40 @@
+//tree structure using depth-first search
+
+//recursive
+function permute(nums) {
+  //global result
+  const result = [];
+
+  //depth for search recursive helper function
+
+  const dfs = (i, nums) => {
+    //base case
+    //if i === nums.length when we are at leaft level of the tree, push the copy of nums to result
+    if (i === nums.length) {
+      result.push(nums.slice());
+      return;
+    }
+
+    //dfs recursive case
+    //swap it to get one permutation and reswap to go up back
+    for (let j = i; j < nums.length; j++) {
+      let temp = nums[i];
+      nums[i] = nums[j];
+      nums[j] = temp;
+      dfs(i + 1, nums);
+      // unswap the positions as we go back up the tree
+      nums[j] = nums[i];
+      nums[i] = temp;
+    }
+  };
+  dfs(0, nums);
+  return result;
+}
+
+console.log(permute([1, 2, 3]));
+
+
+//iterative
+function permute2(){
+
+}
