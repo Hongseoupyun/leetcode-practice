4009d26968afe2bacd24b368841dd695714a00a5
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-09-08 21:36:47 -0400

    fixed small issu in 17.reverseString.js

--- 17.reverseString.js
+++ 17.reverseString.js
@@ -2,13 +2,11 @@
 //loop through it and push the char from the end of the array
 //join it back with .join("")
 function reverse(string) {
-  let stringArray = string.split("");
-  let result = [];
-  console.log(stringArray);
-  for (let i = stringArray.length - 1; i >= 0; i--) {
-    result.push(stringArray[i]);
+  let result = "";
+  for (let i = string.length - 1; i >= 0; i--) {
+    result+=string[i]
   }
-  return result.join("");
+  return result;
 }
 
-console.log(reverse("abcde"));
+console.log(reverse("Reverserthis"));
