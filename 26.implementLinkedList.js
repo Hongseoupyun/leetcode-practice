7dc449fde21259c8dfb404cc9ca39f6dab72c569
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-09-09 18:56:47 -0400

    added append and prepend algorithms to 26.implementLinkedList.js

---
+++ 26.implementLinkedList.js
@@ -0,0 +1,50 @@
+class LinkedList {
+
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  prepend(newValue) {
+    const currentHead = this.head;
+    const newNode = new LinkedListNode(newValue);
+
+    newNode.next = currentHead;
+    this.head = newNode;
+
+    if(!this.tail){
+      this.tail = newNode;
+    }
+  }
+
+  append(newValue) {
+    const newNode = new LinkedListNode(newValue)
+    if(!this.head){
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+  }
+
+  print() {
+
+  }
+
+  getByIndex(index) {
+
+  }
+  insertAtIndex(index, value) {
+
+  }
+  removeAtIndex(index){
+
+  }
+}
+
+function LinkedListNode(value) {
+  this.value = value;
+  this.next = null;
+}
+
