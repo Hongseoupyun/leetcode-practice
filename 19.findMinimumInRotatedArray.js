9365d2d460f5f666a522fa56243318928ff96959
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-09-12 21:48:05 -0400

    commit

--- 19.findMinimumInRotatedArray.js
+++ 19.findMinimumInRotatedArray.js
@@ -4,30 +4,28 @@
 // Output: 1
 // Explanation: The original array was [1,2,3,4,5] rotated 3 times.
 
-
 // [5,1,2,3,4]
 // [4,5,1,2,3]
 // [3,4,5,1,2]
 function findMin(nums) {
-  let left = 0
-  let right = nums.length - 1
-  
-  while (left < right) {   
-    let midPoint = Math.floor((left + right) / 2)
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    let midPoint = Math.floor((left + right) / 2);
     if (nums[midPoint] > nums[right]) {
-      left = midPoint + 1
+      left = midPoint + 1;
     } else {
-      right = midPoint
+      right = midPoint;
     }
   }
-  return nums[left]
+  return nums[left];
 }
-
 
 //O(n)example
 
 function findMin3(nums) {
-  return Math.min(...nums)
+  return Math.min(...nums);
 }
 
-console.log(findMin([5,1,2,3,4]))
+console.log(findMin([5, 1, 2, 3, 4]));
