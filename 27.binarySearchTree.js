2b9bb68180c0df24c415e5a9dd6aca9727b4c485
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-09-12 22:56:57 -0400

    added binarySearchTree.js

---
+++ 27.binarySearchTree.js
@@ -0,0 +1,5 @@
+//binary tree is data structure where the tree has maximum of 2 nodes
+
+//binary search tree is binary tree data structure where each node is greater than every node in its left sub tree and less than every node in its right sub tree.
+
+//Binary search tree is very fast and useful because you can eliminate half of the nodes as you compare nodes. eg) if node that you are fininding it less than root node, you will only find left subtree of the node.
