ef422fd437250a79da49655daf388066d3ad7547
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-09-12 23:36:48 -0400

    added some comments

--- 27.binarySearchTree.js
+++ 27.binarySearchTree.js
@@ -3,3 +3,14 @@
 //binary search tree is binary tree data structure where each node is greater than every node in its left sub tree and less than every node in its right sub tree.
 
 //Binary search tree is very fast and useful because you can eliminate half of the nodes as you compare nodes. eg) if node that you are fininding it less than root node, you will only find left subtree of the node.
+
+//depth first search=>visit nodes vertically first before horizontally :think about stack
+//pre order/ in order/post order
+//pre order => <root> -> <left> -> <right>
+//in order =>  <left> -> <root> -> <right>
+//post order=> <left> -> <right> -> <root>
+
+//breadth first searc(level order traversal)=>visit nodes at the same level first and goes to next level :think about queue
+//level order
+
+
