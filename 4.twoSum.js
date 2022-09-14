61bfc789fc0fd1546c8075b902ba137380be04c0
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-09-14 00:46:08 -0400

    fixed small typo

--- 4.twoSum.js
+++ 4.twoSum.js
@@ -19,7 +19,7 @@ Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 //2.check if the target value - nums[i] is in the hasmap
 //3. if true return [map[target-nums[i]],i]
 //4. otherwise, map[nums[i]]=i
-twoSum = function (nums, target) {
+const twoSum = function (nums, target) {
   let count = {};
 
   for (let i = 0; i < nums.length; i++) {
@@ -35,3 +35,4 @@ twoSum = function (nums, target) {
 };
 
 console.log(twoSum([1, 2, 3, 4, 5, 6, 7], 11));
+
