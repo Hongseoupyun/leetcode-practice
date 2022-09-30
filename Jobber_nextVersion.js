3117467460eddb935dfed99412d95faf1dbf9b12
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-09-29 21:55:20 -0400

    added test file

--- Jobber_nextVersion.js
+++ Jobber_nextVersion.js
@@ -43,3 +43,5 @@ console.log(nextVersion("0.9.9")); //"1.0.0";
 console.log(nextVersion("1")); //"2";
 console.log(nextVersion("1.2.3.4.5.6.7.8")); //"1.2.3.4.5.6.7.9";
 console.log(nextVersion("9.9")); // “10.0";
+
+module.exports = nextVersion;
---
+++ Jobber_nextVersion.test.js
@@ -0,0 +1,3 @@
+const nextVersion = require("./Jobber_nextVersion");
+
+ 
