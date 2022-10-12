09ad715f45051833bcdb2777d993c06c0a8056c5
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-10-12 16:29:21 -0400

    added another solution

--- 9.palindrome.js
+++ 9.palindrome.js
@@ -26,8 +26,8 @@ function isPalindrome(string) {
 
 const palindrome = (str) => {
   for (let i = 0; i < str.length; i++) {
-      if (str[i] !== str[str.length-1 - i])
-          return false;
+    if (str[i] !== str[str.length - 1 - i])
+      return false;
   }
 
   return true;
